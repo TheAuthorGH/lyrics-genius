d3605943291301baecfcb1574006fb53c91df982
Luis Lau <theauthorgm@gmail.com>
2018-08-20 10:58:06 -0500

    expanded analysis content

--- scripts/analysis.js
+++ scripts/analysis.js
@@ -1,15 +1,28 @@
 function lyricsAnalysis(text) {
-	function entry(title, html) {
-		return {title: title, html: html};
+	function entry(title, html, classes) {
+		return {title: title, html: html, classes: classes};
 	};
 	const words = wordArray(text);
 	const wordsUnique = distinct(words);
+	const wordFrequency = frequency(words);
 	const chars = charArray(text);
+	const charFrequency = frequency(chars);
 	return [
+		entry('Lyrics', `
+			<button class="hideable-control"><span class="hideable-hidden">Show</span><span class="hideable-shown">Hide</span></button>
+			<p class="hideable-content track-lyrics">${text.replace(new RegExp('\n', 'g'), '<br>')}</p>
+		`, 'hideable column'),
 		entry('Words', words.length),
 		entry('Words (Unique)', wordsUnique.length),
 		entry('Monotony', formatDecimal(words.length / wordsUnique.length, 2)),
-		entry('Most Frequent Word', frequency(words)[0].item),
-		entry('Characters', chars.length)
+		entry('Common Words', `
+			<button class="hideable-control"><span class="hideable-hidden">Show</span><span class="hideable-shown">Hide</span></button>
+			<table class="hideable-content">
+				<tr><th>Word</th><th>Count</th></tr>
+				${wordFrequency.slice(0, 10).reduce((e, f) => e + ('<tr><td>' + f.item + '</td><td>' + f.frequency + '</td></tr>'), '')}
+			</table>
+		`, 'hideable column'),
+		entry('Characters', chars.length),
+		entry('Most Frequent Character', charFrequency[0].item)
 	];
 }
